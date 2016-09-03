8d08556db8c12ca1a8e7eb0dc17dccd8abc5ef97
SamYao <yaosam@yeah.net>
2016-09-03 08:44:43 +0800

    initial

---
+++ basic/main.cpp
@@ -0,0 +1,55 @@
+/*
+*　　　　　　　　┏┓　　　┏┓+ +
+*　　　　　　　┏┛┻━━━┛┻┓ + +
+*　　　　　　　┃　　　　　　　┃ 　
+*　　　　　　　┃　　　━　　　┃ ++ + + +
+*　　　　　　 ████━████ ┃+
+*　　　　　　　┃　　　　　　　┃ +
+*　　　　　　　┃　　　┻　　　┃
+*　　　　　　　┃　　　　　　　┃ + +
+*　　　　　　　┗━┓　　　┏━┛
+*　　　　　　　　　┃　　　┃　　　　　　　　　　　
+*　　　　　　　　　┃　　　┃ + + + +
+*　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+*　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+*　　　　　　　　　┃　　　┃
+*　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+*　　　　　　　　　┃　 　　┗━━━┓ + +
+*　　　　　　　　　┃ 　　　　　　　┣┓
+*　　　　　　　　　┃ 　　　　　　　┏┛
+*　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+*　　　　　　　　　　┃┫┫　┃┫┫
+*　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+*/
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+#undef re
+#define re(i,n) for(unsigned int i=0;i<n;i++)
+#define DEBUG
+
+
+int main()
+{
+	clock_t BeginTime = clock();
+	srand(unsigned(time(NULL)));
+#ifdef DEBUG
+	FILE *input, *output; //没用的指针... ...
+	freopen_s(&input, "in.txt", "r", stdin);
+	freopen_s(&output, "out.txt", "w", stdout);
+#endif
+	try {
+		/*主函数*/
+
+
+
+	}
+	catch (const char * error) { cout << error << endl; }
+	cout << "运行时间：" << clock() - BeginTime << endl;
+#ifndef DEBUG
+	system("pause");
+#endif // !DEBUG
+	return 0;
+}
